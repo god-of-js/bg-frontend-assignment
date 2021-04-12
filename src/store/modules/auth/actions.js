d000da814e251078a5ac7bcba21846c06352b6e9
godofjs <henryeze019@gmail.com>
2021-04-12 13:54:17 +0100

    final push

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -9,10 +9,12 @@ export default {
           commit(`user/${SET_USER_DATA}`, data, { root: true });
           localStorage.setItem("userData", JSON.stringify(data.user));
           localStorage.setItem("userToken", JSON.stringify(data.token));
+          // A resolve is used here to notify the component that the process has been completed and it can now be routed to the dashboard.
           resolve(data);
         })
         .catch((err) => {
           alert(err.data.message);
+          // Reject is used here to alert the component of a failure so the loader could be stopped.
           reject(err);
         });
     });
